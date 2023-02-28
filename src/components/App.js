ab980e3cce99b9cc8bc0ace0908ae64abb6be541
alex lieberman <alex.lieberman182@gmail.com>
2023-02-28 07:11:18 -0800

    adding in some hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 function App() {
+    const [info, setInfo] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:4000/tacos')
+            .then((r)=>r.json())
+            .then((stuff)=>setInfo(stuff))
+    }, [])
+
+    console.log(info)
+
     return(
-        <h1>Hello World!</h1>
+        info.map(i=><h1>{i.title}</h1>)
     )
 }
 export default App
