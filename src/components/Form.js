910855d327d59c81c715f6db3e75f9e82279900a
alex lieberman <alex.lieberman182@gmail.com>
2023-03-01 08:34:48 -0800

    added header to form

--- src/components/Form.js
+++ src/components/Form.js
@@ -48,17 +48,20 @@ function Form() {
     }
 
     return (
-        <form onSubmit={handleSubmit}>
-            <label>Doublespeak Phrase</label>
-            <input type='text' value={jargon} onChange={handleJargonChange}/>
-            <label>Definition</label>
-            <input type='text' value={definition} onChange={handleDefinitionChange}/>
-            <label>Use the phrase in a sentence</label>
-            <input type='text' value={sentence} onChange={handleSentenceChange} />
-            <label>Gif or image link</label>
-            <input type='text' value={image} onChange={handleImageChange} />
-            <button type='submit'>Submit</button>
-        </form>
+        <div>
+            <h1>Add you new term here</h1>
+            <form onSubmit={handleSubmit}>
+                <label>Doublespeak Phrase</label>
+                <input type='text' value={jargon} onChange={handleJargonChange}/>
+                <label>Definition</label>
+                <input type='text' value={definition} onChange={handleDefinitionChange}/>
+                <label>Use the phrase in a sentence</label>
+                <input type='text' value={sentence} onChange={handleSentenceChange} />
+                <label>Gif or image link</label>
+                <input type='text' value={image} onChange={handleImageChange} />
+                <button type='submit'>Submit</button>
+            </form>
+        </div>
     )
 }
 
